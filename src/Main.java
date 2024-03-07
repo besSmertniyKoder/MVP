5769019eea71c0788a1e95f74ed2af18a405f7a1
Roman Sukhachev <rsukhachev@bostonsd.ru>
2024-03-07 10:45:14 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import controller.OnlineStoreController;
+import model.Orders;
+import model.Shwarma;
+import view.StoreView;
+
+public class Main {
+    public static void main(String[] args) {
+        OnlineStoreController onlineStoreController =new OnlineStoreController();
+        onlineStoreController.addOrder(new Shwarma(200, "сырный лаваш и много овощей/мяса"));
+        StoreView storeView = new StoreView(onlineStoreController);
+        System.out.println("////////////////");
+        storeView.showOrders();
+
+        System.out.println();
+
+    }
+}
